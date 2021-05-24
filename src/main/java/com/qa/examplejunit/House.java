598fb5b529634e3f8298b9d16ee5f2028209bd34
skeletoro <69654563+skeletoro@users.noreply.github.com>
2021-05-24 09:16:27 -0700

    zh uploading maven junit practice

---
+++ src/main/java/com/qa/examplejunit/House.java
@@ -0,0 +1,5 @@
+package com.qa.examplejunit;
+
+public class House {
+
+}
---
+++ src/test/java/com/qa/examplejunit/HouseTest.java
@@ -0,0 +1,12 @@
+package com.qa.examplejunit;
+
+import org.junit.jupiter.api.Test;
+
+public class HouseTest {
+	
+	@Test
+	public void test1() {
+		
+	}
+
+}
